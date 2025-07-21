4f07a2d6d08593916f2dab1c1ba3543f992c837d
leafworthy <51889564+leafworthy@users.noreply.github.com>
2025-07-21 02:14:43 -0700

    Add tintable color replacement shaders for enemies
    - Updated PalletteSwapper script
    - Modified enemy prefabs (Corn, Donut, Toast) to use new shader system
    - Added TintableColorReplace shader graphs (MAX and MAXXXX variants)
    - Created material folders for Corn, Donut, and Toast enemies
    - Updated test sidescroll scene with shader changes

--- Assets/+SCRIPTS/PalletteSwapper.cs
+++ Assets/+SCRIPTS/PalletteSwapper.cs
@@ -6,28 +6,26 @@ using VInspector;
 [ExecuteAlways]
 public class PalletteSwapper : MonoBehaviour, IPoolable
 {
-public List<Material> pallettes = new List<Material>();
-public SpriteRenderer spriteRenderer;
+	public List<Material> pallettes = new();
+	public SpriteRenderer spriteRenderer;
 
-    public void OnPoolSpawn()
-    {
-        spriteRenderer.material = pallettes[Random.Range(0, pallettes.Count)];
-    }
+	private void Start()
+	{
+		Swap();
+	}
 
-    public void SetPallette(Material mat)
-    {
-        if (spriteRenderer != null)
-        {
-            spriteRenderer.material = mat;
-        }
-    }
+	public void OnPoolSpawn()
+	{
+		Swap();
+	}
 
-    [Button]
-    public void Swap()
-    {
-        spriteRenderer.material = pallettes[Random.Range(0, pallettes.Count)];
-    }
-    public void OnPoolDespawn()
-    {
-    }
+	[Button]
+	public void Swap()
+	{
+		spriteRenderer.material = pallettes[Random.Range(0, pallettes.Count)];
+	}
+
+	public void OnPoolDespawn()
+	{
+	}
 }
