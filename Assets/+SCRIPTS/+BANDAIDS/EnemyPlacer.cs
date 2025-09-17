81793392cf03fa72527322086212b7f9cb313f36
leafworthy <51889564+leafworthy@users.noreply.github.com>
2025-09-16 23:36:12 -0700

    placers spawn when on screen

--- Assets/+SCRIPTS/+BANDAIDS/EnemyPlacer.cs
+++ Assets/+SCRIPTS/+BANDAIDS/EnemyPlacer.cs
@@ -1,3 +1,4 @@
+using __SCRIPTS.Cursor;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -5,12 +6,27 @@ using UnityEngine;
 public class EnemyPlacer : MonoBehaviour
 {
 	public GameObject EnemyPrefab;
+	public Camera cam;
 
 	private void Start()
+	{
+		cam = CursorManager.GetCamera();
+	}
+
+	private void Spawn()
 	{
 		Services.enemyManager.SpawnNewEnemy(EnemyPrefab, transform.position);
 		Destroy(gameObject);
 	}
 
+	private void Update()
+	{
+		if (IsOnScreen()) Spawn();
+	}
 
+	private bool IsOnScreen()
+	{
+		Vector3 viewportPos = cam.WorldToViewportPoint(transform.position);
+		return viewportPos.x >= 0 && viewportPos.x <= 1 && viewportPos.y >= 0 && viewportPos.y <= 1 && viewportPos.z >= 0;
+	}
 }
